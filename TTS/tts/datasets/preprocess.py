3cfc6884c94c1427b8c9dda9f156a58abe725655
stevenguyen72 <stevenguyen7296@gmail.com>
2021-07-13 09:31:07 +0700

    add function myself to dataset.py

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -68,6 +68,20 @@ def get_preprocessor_by_name(name):
 # DATASETS
 ########################
 
+def myself(root_path, meta_file):
+    """Normalizes the Nancy meta data file to TTS format"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "ljspeech"
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split('\t')
+            wav_file = os.path.join(root_path, cols[0])
+            text = cols[1]
+            items.append([text, wav_file, speaker_name])
+    return items
+
+
 def tweb(root_path, meta_file):
     """Normalize TWEB dataset.
     https://www.kaggle.com/bryanpark/the-world-english-bible-speech-dataset
